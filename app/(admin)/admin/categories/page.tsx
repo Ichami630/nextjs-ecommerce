ec0046c4f32e2a68d191ea68cd2a2762e5179b33
ichami630 <brandonichami@gmail.com>
2025-02-21 03:36:11 +0100

    added a suspense boundary for the admin categories page to see if dep problem can be solved

--- app/(admin)/admin/categories/page.tsx
+++ app/(admin)/admin/categories/page.tsx
@@ -4,13 +4,14 @@ import Table from "../../../../components/admin/Table"
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import { Plus } from 'lucide-react';
+import { Suspense } from 'react';
 
 const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 70 },
     { field: 'categoryName', headerName: 'Category Name', width: 200 },
     { field: 'slug', headerName: 'Slug', width: 200 },
     { field: 'dateAdded', headerName: 'Date Added', width: 150 },
-  ];
+];
 
 const rows = [
     { id: 1, categoryName: 'Used Cars', slug: 'used-cars', dateAdded: '2024-02-18' },
@@ -21,35 +22,44 @@ const Page = () => {
     const searchParams = useSearchParams();
     const categoryId = searchParams.get('id');
     const isEditing = !!categoryId;
-  return (
-    <div className="flex flex-col space-y-10">
-        <div className="flex p-4 flex-col space-y-4 shadow-md bg-white">
-            <div className="flex flex-row space-x-2 items-center">
-                <div className="text-2xl text-red-700 font-extrabold">
-                {isEditing ? "Existing Category": "New Category"}
+
+    return (
+        <div className="flex flex-col space-y-10">
+            <div className="flex p-4 flex-col space-y-4 shadow-md bg-white">
+                <div className="flex flex-row space-x-2 items-center">
+                    <div className="text-2xl text-red-700 font-extrabold">
+                        {isEditing ? "Existing Category" : "New Category"}
+                    </div>
+                    <Link title='new category' href="/admin/categories" className='border-2 bg-gray-400 p-1'>
+                        <Plus size={14} />
+                    </Link>
                 </div>
-                <Link title='new category' href="/admin/categories" className='border-2 bg-gray-400 p-1'><Plus size={14} /></Link>
+                <form action="" className="mt-4">
+                    <div className="flex flex-col md:flex-row justify-between gap-4">
+                        <div className="flex flex-col space-y-1 w-full">
+                            <label htmlFor="categoryname" className='text-gray-400'>Category Name <span className="text-red-600">*</span></label>
+                            <input type="text" name="" id="categoryName" className="border focus:outline-none w-full rounded-md p-2" />
+                        </div>
+                        <div className="flex flex-col space-y-1 w-full">
+                            <label htmlFor="categoryslug" className='text-gray-400'>Category Slug <span className="text-red-600">*</span></label>
+                            <input type="text" name="" id="categoryName" className="border focus:outline-none w-full rounded-md p-2" placeholder='e.g: used-cars' />
+                        </div>
+                    </div>
+                    <button type="submit" className="p-2 font-bold bg-primary text-black hover:bg-black hover:text-white rounded-md mt-4">
+                        {isEditing ? "Update category" : "Save category"}
+                    </button>
+                </form>
             </div>
-            <form action="" className="mt-4">
-                <div className="flex flex-col md:flex-row justify-between gap-4">
-                    <div className="flex flex-col space-y-1 w-full">
-                        <label htmlFor="categoryname" className='text-gray-400'>Category Name <span className="text-red-600">*</span></label>
-                        <input type="text" name="" id="categoryName" className="border focus:outline-none w-full rounded-md p-2" />
-                    </div>
-                    <div className="flex flex-col space-y-1 w-full">
-                        <label htmlFor="categoryslug" className='text-gray-400'>Category Slug <span className="text-red-600">*</span></label>
-                        <input type="text" name="" id="categoryName" className="border focus:outline-none w-full rounded-md p-2" placeholder='e.g: used-cars' />
-                    </div>
+            {/*existing categories */}
+            <Suspense fallback={<div className="fixed inset-0 flex justify-center items-center bg-gray-500 bg-opacity-50 z-40">
+                <div className="text-white">Loading...</div>
+                </div>}>
+                <div className="flex flex-col shadow-md p-4 space-y-4 bg-white">
+                    <Table title="Existing Categories" editRoute='/admin/categories' rows={rows} columns={columns} deleteEndpoint="/api/categories/delete" />
                 </div>
-                <button type="submit" className="p-2 font-bold bg-primary text-black hover:bg-black hover:text-white rounded-md mt-4">{isEditing ? "Update category": "Save category"}</button>
-            </form>
+            </Suspense>
         </div>
-        {/*existing categories */}
-        <div className="flex flex-col shadow-md p-4 space-y-4 bg-white overflow-auto">
-            <Table title="Existing Categories" editRoute='/admin/categories' rows={rows} columns={columns} deleteEndpoint="/api/categories/delete" />
-        </div>
-    </div>
-  )
+    )
 }
 
-export default Page
+export default Page;
--- components/admin/Table.tsx
+++ components/admin/Table.tsx
@@ -47,7 +47,7 @@ const Table: React.FC<TableProps> = ({ title, rows, columns, editRoute,deleteEnd
     <>
       <div className="text-xl text-red-600 font-bold mb-4">{title}</div>
 
-      <Box sx={{ height: 400, width: '100%' }}>
+      <Box sx={{ height: 400, width: '100%',zIndex: 20 }}>
         {selectedRows.length > 0 && (
           <Button
             variant="contained"
@@ -60,6 +60,7 @@ const Table: React.FC<TableProps> = ({ title, rows, columns, editRoute,deleteEnd
           </Button>
         )}
 
+        <div className="overflow-x-auto">
         <DataGrid
           rows={rows}
           columns={[
@@ -90,6 +91,7 @@ const Table: React.FC<TableProps> = ({ title, rows, columns, editRoute,deleteEnd
             setSelectedRows(newSelection as number[]);
           }}
         />
+        </div>
       </Box>
     </>
   );
